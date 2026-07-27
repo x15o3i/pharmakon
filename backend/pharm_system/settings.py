29e840e68e62e1fd4ed8174dd1e6c9053692bf35
Raphael Fulfilled <raphaelfulfilled@gmail.com>
2026-07-28 00:36:35 +0100

    Optimize full-stack project for Vercel deployment with Whitenoise, serverless WSGI app entrypoint, and vercel.json rewrites

--- backend/pharm_system/settings.py
+++ backend/pharm_system/settings.py
@@ -16,6 +16,7 @@ DEBUG = os.getenv('DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
+    'https://*.vercel.app',
     'https://*.ngrok-free.app',
     'https://*.ngrok.io',
     'http://localhost:5173',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,6 +129,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Django REST Framework Settings
